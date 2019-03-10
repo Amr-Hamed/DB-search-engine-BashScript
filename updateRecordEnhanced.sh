5b93356e15c3c3dd17e27acfaaf86473336319fd
Mohammed Gomaa <promgom3a@gmail.com>
2019-03-11 00:37:44 +0200

    enhance display , delete and update record functions

--- updateRecordEnhanced.sh
+++ updateRecordEnhanced.sh
@@ -27,18 +27,18 @@ function updateRecordEnhanced(){
 
         if [ ${tableMetaArray[0]} == $PKName ]
         then
-            if [[ $(cut -d":" -f 2 ./DBs/$DBName/$tableName | grep $PKVal) == $PKVal ]] 
+            if [[ $(awk -F: -v pk=$PKVal '{if(pk==$2&&NR>1) print $0}' ./DBs/$DBName/$tableName) ]] 
             then
                 oldRecord=$(awk -F: -v pk=$PKVal '{if(pk==$2) print $0}' ./DBs/$DBName/$tableName)
                 IFS=':' read -ra oldRecordArray <<< $oldRecord
 
                 for ((i=0;i<${tableMetaArray[-1]};i+=2))
                 do
-                    echo ${tableMetaArray[i]}
+                    
                     j=3
                     for ((;j<=$#;j++)) {
                         flag=$(case "${tableMetaArray[@]}" in  ("${!j} "*|*" ${!j} "*|*" ${!j}") echo "${!j}" ;; esac)
-                        echo $flag
+                        
                         if [[ $flag ]] 
                         then
                             if [[ ${tableMetaArray[i]} == ${!j} ]]; 
